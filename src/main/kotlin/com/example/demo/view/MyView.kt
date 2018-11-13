23b14dba18bc9e673329394549c50428319b5606
Niccolò Maltoni <nicco.mlt@gmail.com>
2018-11-13 20:50:29 +0100

    Implement minimal view with a controller

--- src/main/kotlin/com/example/demo/view/MyView.kt
+++ src/main/kotlin/com/example/demo/view/MyView.kt
@@ -1,13 +1,40 @@
 package com.example.demo.view
 
+import javafx.beans.property.SimpleStringProperty
+import tornadofx.Controller
 import tornadofx.View
+import tornadofx.action
 import tornadofx.button
+import tornadofx.field
+import tornadofx.fieldset
+import tornadofx.form
 import tornadofx.label
+import tornadofx.textfield
 import tornadofx.vbox
 
 class MyView : View() {
-    override val root = vbox {
-        button("Press me")
-        label("Waiting")
+    val controller: MyController by inject()
+    val input = SimpleStringProperty()
+
+    override val root = form {
+        fieldset {
+            field("Input") {
+                textfield(input)
+            }
+
+            button("Commit") {
+                action {
+                    controller.writeToDb(input.value)
+                    input.value = ""
+                }
+            }
+        }
     }
 }
+
+class MyController: Controller() {
+    fun writeToDb(inputValue: String) {
+        println("Writing $inputValue to database!")
+    }
+}
+
