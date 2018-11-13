39682e74b73f984d679070b82fe0283027bd82e7
Niccolò Maltoni <nicco.mlt@gmail.com>
2018-11-13 16:30:17 +0100

    Define MasterView which embeds two views directly

---
+++ src/main/kotlin/com/example/demo/view/MasterView.kt
@@ -0,0 +1,20 @@
+package com.example.demo.view
+
+import tornadofx.View
+import tornadofx.borderpane
+import tornadofx.label
+
+class MasterView: View() {
+    override val root = borderpane {
+        top<TopView>()
+        bottom<BottomView>()
+    }
+}
+
+class TopView: View() {
+    override val root = label("Top View")
+}
+
+class BottomView: View() {
+    override val root = label("Bottom View")
+}
