b3a42d1869c5cd0caf710cd9c9b93fbe64d9492e
Niccolò Maltoni <nicco.mlt@gmail.com>
2018-11-14 17:41:24 +0100

    Do Fragment and View with openModal

---
+++ src/main/kotlin/com/example/demo/view/MyView.kt
@@ -0,0 +1,34 @@
+package com.example.demo.view
+
+import javafx.stage.StageStyle
+import tornadofx.Fragment
+import tornadofx.View
+import tornadofx.action
+import tornadofx.button
+import tornadofx.label
+import tornadofx.vbox
+
+class MyView : View() {
+    override val root = vbox {
+        button("Press Me") {
+            action {
+                // Both View and Fragment support openModal(), openWindow(), and openInternalWindow() functions
+                // that will open the root node in a separate Window
+                find<MyFragment>().openModal(stageStyle = StageStyle.UTILITY)
+                /*
+                 * Optional Arguments for openModal():
+                 * stageStyle           -> StageStyle   -> Defines one of the possible enum styles for Stage
+                 * modality             -> Modality     -> Defines one of the possible enum modality types for Stage
+                 * escapeClosesWindow   -> Boolean      -> Sets the ESC key to call close(). Default: true
+                 * owner                -> Window       -> Specify the owner Window for this Stage
+                 * block                -> Boolean      -> Block UI execution until the Window closes. Default: false
+                 */
+            }
+        }
+    }
+}
+
+/** A Fragment is a special type of View designed to have multiple instances */
+class MyFragment: Fragment() {
+    override val root = label("This is a popup")
+}
