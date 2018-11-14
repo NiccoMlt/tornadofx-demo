d1cd7a205415ba35e0697f5c6b3a6e44229c86f9
Niccolò Maltoni <nicco.mlt@gmail.com>
2018-11-14 17:45:11 +0100

    Do Fragment and View with openInternalWindow for MDI-like GUI

--- src/main/kotlin/com/example/demo/view/MyView.kt
+++ src/main/kotlin/com/example/demo/view/MyView.kt
@@ -1,6 +1,5 @@
 package com.example.demo.view
 
-import javafx.stage.StageStyle
 import tornadofx.Fragment
 import tornadofx.View
 import tornadofx.action
@@ -12,16 +11,18 @@ class MyView : View() {
     override val root = vbox {
         button("Press Me") {
             action {
-                // Both View and Fragment support openModal(), openWindow(), and openInternalWindow() functions
-                // that will open the root node in a separate Window
-                find<MyFragment>().openModal(stageStyle = StageStyle.UTILITY)
+                // While openModal opens in a new Stage, openInternalWindow opens over the current root node,
+                // or any other node if you specify it
+                openInternalWindow<MyFragment>()
                 /*
-                 * Optional Arguments for openModal():
-                 * stageStyle           -> StageStyle   -> Defines one of the possible enum styles for Stage
-                 * modality             -> Modality     -> Defines one of the possible enum modality types for Stage
+                 * Optional Arguments for openInternalWindow()
+                 * view	                -> UIComponent  -> The component will be the content of the new window
+                 * view                 -> KClass       -> Alternatively, the class of the view instead of an instance
+                 * icon                 -> Node         -> Optional window icon
+                 * scope                -> Scope        -> If you specify the view class, you can also specify the scope
+                 * modal                -> Boolean      -> If the node should be disabled while the window is active
                  * escapeClosesWindow   -> Boolean      -> Sets the ESC key to call close(). Default: true
-                 * owner                -> Window       -> Specify the owner Window for this Stage
-                 * block                -> Boolean      -> Block UI execution until the Window closes. Default: false
+                 * owner                -> Node         -> Specify the owner Node for this window
                  */
             }
         }
@@ -29,6 +30,6 @@ class MyView : View() {
 }
 
 /** A Fragment is a special type of View designed to have multiple instances */
-class MyFragment: Fragment() {
+class MyFragment : Fragment() {
     override val root = label("This is a popup")
 }
